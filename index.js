2639c6c43390f23678a5d9e6f28489acf709b8eb
tarvainen <tarvainen.atte@gmail.com>
2019-10-05 13:00:07 +0300

    Add swagger doc

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const ruuvi = require('node-ruuvitag')
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const yaml = require('js-yaml')
+const fs = require('fs')
+
 const app = express()
+const swaggerDoc = yaml.safeLoad(fs.readFileSync('./swagger.yml', 'utf8'))
 
 const tags = []
 const latest = {}
@@ -26,4 +31,11 @@ app.get('/tag/:id', (req, res) => {
   return res.json(latest[id])
 })
 
-app.listen(process.env.PORT || 3000, err => err && console.error(err))
+const port = process.env.PORT || 3000
+const base = `http://localhost:${port}`
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+console.log(`Swagger API documentation available at ${base}/doc`)
+
+app.listen(port, err => err ? console.error(err) : console.log(`App listening on ${base}`))
